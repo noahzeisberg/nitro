524f7e02b8a42e0660c3dbcebc85ea36327014bf
NoahOnFyre <noahonfyre@gmail.com>
2023-07-16 17:58:26 +0200

    Fixed installer.

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@
 title Installing Scorpion...
 
 echo Installing Scorpion...
-copy dist/scorp.exe C:\Windows\System32
-copy dist/scorpion.exe C:\Windows\System32
+copy dist\scorp.exe C:\Windows\System32
+copy dist\scorpion.exe C:\Windows\System32
 scorp
