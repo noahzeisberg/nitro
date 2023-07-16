329030a543fa37e0ece3d8f97da9b88d0cac5367
NoahOnFyre <noahonfyre@gmail.com>
2023-07-16 20:43:34 +0200

    Added some more functionality.

--- scorp.py
+++ scorp.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import shutil
+import subprocess
 import sys
 from pathlib import Path
 
@@ -46,52 +47,9 @@ def valid_args(count: int):
         return True
 
 
-def check_package(pkg: str):
-    if not pkg.__contains__("/"):
-        global package
-        package = "NoahOnFyre/" + pkg
-
-
-def menu():
-    os.system("title Scorpion - Async Package Manager")
-    print(Fore.GREEN + """   _____                       _           
-  / ___/_________  _________  (_)___  ____ 
-  \__ \/ ___/ __ \/ ___/ __ \/ / __ \/ __ \\
- ___/ / /__/ /_/ / /  / /_/ / / /_/ / / / /
-/____/\___/\____/_/  / .___/_/\____/_/ /_/ 
-                    /_/                                    """)
-
-
-menu()
-path = os.path.dirname(os.path.abspath(sys.argv[0]))
-out_dir = str(Path.home()) + "\\.scorpion\\"
-
-if len(sys.argv) > 1:
-    script = sys.argv[1]
-    os.system("title Scorpion - Running: " + script)
-    print()
-    print(Fore.LIGHTBLACK_EX + "\\\\" + Fore.GREEN + "scorpion" + Fore.LIGHTBLACK_EX + " >>> " + Fore.RESET + "run " + script)
-    print()
-    if os.path.exists(out_dir + script):
-        with open(out_dir + script + "\\main.py", "rb") as file:
-            exec(file.read())
-    else:
-        os.system(script)
-    sys.exit()
-
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-
-while True:
-    try:
-        print()
-        args = input(Fore.LIGHTBLACK_EX + "\\\\" + Fore.GREEN + "scorpion" + Fore.LIGHTBLACK_EX + " >>> " + Fore.RESET).split(" ")
-        print()
-        cmd = args[0]
-        args.remove(cmd)
-    except KeyboardInterrupt:
-        sys.exit(0)
-
+def init(cmd, args):
+    global package
+    global target_path
     try:
         match cmd:
             case "install" | "get" | "add":
@@ -105,12 +63,18 @@ while True:
                     content = requests.get("https://api.github.com/repos/" + package + "/contents").json()
                     asyncio.run(start(content))
 
+            case "list":
+                for file in os.listdir(out_dir):
+                    print(prefix() + Fore.GREEN + file.removeprefix(out_dir))
+
+            case "dir":
+                os.system("start explorer.exe " + out_dir)
+
             case "run":
                 if valid_args(1):
                     script = args[0]
-                    if os.path.exists(out_dir + script):
-                        with open(out_dir + script + "\\main.py", "rb") as file:
-                            exec(file.read())
+                    if os.path.exists(out_dir + "\\" + script):
+                        subprocess.call("python " + out_dir + "\\" + script + "\\main.py")
                     else:
                         os.system(script)
 
@@ -134,6 +98,7 @@ while True:
                     repository = package.split("/")[1]
                     target_path = out_dir + "\\" + repository
                     shutil.rmtree(target_path)
+                    print(prefix() + "Successfully removed package!")
 
             case "exit":
                 sys.exit(0)
@@ -146,7 +111,55 @@ while True:
                 os.system("start " + path + "\\scorp.py")
                 sys.exit(0)
             case _:
-                print(prefix("ERROR") + "Unknown command.")
+                if os.path.exists(out_dir + "\\" + cmd):
+                    subprocess.call("python " + out_dir + "\\" + cmd + "\\main.py")
+                else:
+                    print(prefix("ERROR") + "Command not found.")
+
     except Exception as e:
         print(prefix("ERROR") + str(e))
-        continue
+
+
+def check_package(pkg: str):
+    if not pkg.__contains__("/"):
+        global package
+        package = "NoahOnFyre/" + pkg
+
+
+def menu():
+    os.system("title Scorpion - Async Package Manager")
+    print(Fore.GREEN + """   _____                       _           
+  / ___/_________  _________  (_)___  ____ 
+  \__ \/ ___/ __ \/ ___/ __ \/ / __ \/ __ \\
+ ___/ / /__/ /_/ / /  / /_/ / / /_/ / / / /
+/____/\___/\____/_/  / .___/_/\____/_/ /_/ 
+                    /_/                                    """)
+
+
+path = os.path.dirname(os.path.abspath(sys.argv[0]))
+out_dir = str(Path.home()) + "\\.scorpion"
+
+if len(sys.argv) > 1:
+    args = sys.argv
+    cmd = args[1]
+    args.remove(sys.argv[0])
+    args.remove(cmd)
+    print()
+    init(cmd=cmd, args=args)
+    sys.exit(0)
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+menu()
+while True:
+    try:
+        print()
+        args = input(Fore.LIGHTBLACK_EX + "\\\\" + Fore.GREEN + "scorpion" + Fore.LIGHTBLACK_EX + " >>> " + Fore.RESET).split(" ")
+        print()
+        cmd = args[0]
+        args.remove(cmd)
+    except KeyboardInterrupt:
+        sys.exit(0)
+    init(cmd=cmd, args=args)
+
