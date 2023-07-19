52ddc5186b7ba66f6b104a8535fcd15092348078
NoahOnFyre <noahonfyre@gmail.com>
2023-07-19 20:17:11 +0200

    Made nitro "real" async.

--- nitro.py
+++ nitro.py
@@ -4,11 +4,12 @@ import os
 import shutil
 import subprocess
 import sys
-import time
 from pathlib import Path
+import platform
 
-import packaging.version
+import packaging.version as version_parser
 import requests
+import requests_futures.sessions
 from colorama import Fore, Back, init
 
 init(convert=True)
@@ -20,12 +21,6 @@ def fetch(dataset):
     content = requests.get(dataset["download_url"]).content
     with open(target_path + "\\" + dataset["name"], mode="wb") as file:
         file.write(content)
-    print(prefix() + "Fetched " + Fore.GREEN + dataset["name"] + Fore.RESET + f"!" + Fore.LIGHTBLACK_EX + " (" + str(dataset["size"]) + " Bytes)")
-
-
-async def start(content: str):
-    for entry in content:
-        await asyncio.to_thread(fetch, entry)
 
 
 def prefix(level: str = "INFO"):
@@ -48,7 +43,7 @@ def print_help_entry(command_name: str, description: str):
     print(prefix() + first + second)
 
 
-def valid_args(count: int):
+def valid_args(args, count: int):
     if len(args) < count:
         print(prefix("ERROR") + "Invalid arguments!")
         return False
@@ -56,45 +51,54 @@ def valid_args(count: int):
         return True
 
 
-def init(cmd, args):
+async def handle_command(cmd, args):
     global package
     global target_path
     try:
         match cmd:
             case "install" | "get":
-                if valid_args(1):
-                    package = args[0]
+                if valid_args(args, 1):
+                    package = args[0].lower()
                     check_package(package)
                     user = package.split("/")[0]
                     repository = package.split("/")[1]
                     target_path = out_dir + "\\" + repository
                     os.makedirs(target_path)
                     content = requests.get("https://api.github.com/repos/" + package + "/contents").json()
-                    asyncio.run(start(content))
-                    print(prefix() + "Verifying collected artifacts...")
+                    print(prefix() + "Fetching content of " + Fore.GREEN + package + Fore.RESET + "...")
+                    tasks = []
+                    for entry in content:
+                        tasks.append(asyncio.to_thread(fetch, entry))
+                    await asyncio.gather(*tasks)
+                    print(prefix() + "All artifacts collected!")
+                    print(prefix() + "Verifying...")
                     artifacts = os.listdir(target_path)
-                    for a in artifacts:
-                        a.removeprefix(target_path)
-                    if not artifacts.__contains__("main.py"):
-                        print(prefix("ERROR") + "No main file found!")
-                        main_file = input(prefix("INFO") + "Enter name of main file: ")
-                    else:
-                        main_file = "main.py"
-                    print(prefix() + "Creating manifest...")
+                    includes_manifest = False
+                    for artifact in artifacts:
+                        artifact.removeprefix(target_path)
+                        if artifact == "manifest.nitro":
+                            print(prefix() + "Found project manifests!")
+                            includes_manifest = True
 
-                    with open(target_path + "\\manifest.nitro", "x") as file:
-                        file.write(json.dumps({
-                            "repository": package,
-                            "path": target_path,
-                            "main": main_file
-                        }, indent=4))
+                    if includes_manifest:
+                        print(prefix() + "Using project manifests.")
+                    else:
+                        print(prefix() + "Creating manifest...")
+
+                        with open(target_path + "\\manifest.nitro", "x") as file:
+                            file.write(json.dumps({
+                                "repository": package,
+                                "author": user,
+                                "path": target_path
+                            }, indent=4))
+                    print(prefix() + "Successfully collected package " + Fore.GREEN + package + Fore.RESET + "!")
 
             case "list":
                 for file in os.listdir(out_dir):
                     print(prefix() + Fore.GREEN + file.removeprefix(out_dir))
 
             case "run":
-                if valid_args(1):
+                if valid_args(args, 1):
                     script = args[0]
                     if os.path.exists(out_dir + "\\" + script):
                         subprocess.call("python " + out_dir + "\\" + script + "\\main.py")
@@ -102,25 +106,19 @@ def init(cmd, args):
                         os.system(script)
 
             case "update":
-                if valid_args(1):
-                    package = args[0]
-                    check_package(package)
-                    user = package.split("/")[0]
-                    repository = package.split("/")[1]
-                    target_path = out_dir + "\\" + repository
-                    shutil.rmtree(target_path)
-                    os.makedirs(target_path)
-                    content = requests.get("https://api.github.com/repos/" + package + "/contents").json()
-                    asyncio.run(start(content))
+                current_version = version_parser.parse(version)
+                target_version = version_parser.parse("")
+                if current_version < target_version:
+                    print(prefix() + "Downloading update...")
 
             case "uninstall" | "remove":
-                if valid_args(1):
-                    package = args[0]
+                if valid_args(args, 1):
+                    package = args[0].lower()
                     check_package(package)
                     user = package.split("/")[0]
                     repository = package.split("/")[1]
                     target_path = out_dir + "\\" + repository
-                    shutil.rmtree(target_path)
+                    await asyncio.to_thread(lambda: shutil.rmtree(target_path))
                     print(prefix() + "Successfully removed package!")
 
             case "help":
@@ -128,7 +126,7 @@ def init(cmd, args):
                 print_help_entry("get", "Fetch a package and save it to local storage.")
                 print_help_entry("list", "List all installed packages.")
                 print_help_entry("run", "Run a package. (only python supported)")
-                print_help_entry("update", "Re-fetch a package and save it to local storage.")
+                print_help_entry("update", "Update your Nitro instance to the newest version.")
                 print_help_entry("remove", "Remove a package from local storage.")
                 print_help_entry("help", "Get help about the commands.")
                 print_help_entry("dir", "Open the location of your packages.")
@@ -151,7 +149,7 @@ def init(cmd, args):
                 sys.exit(0)
 
             case _:
-                if os.path.exists(out_dir + "\\" + cmd):
+                if os.path.exists(out_dir + "\\" + cmd + "\\"):
                     subprocess.call("python " + out_dir + "\\" + cmd + "\\main.py")
                 else:
                     print(prefix("ERROR") + "Command not found.")
@@ -163,43 +161,47 @@ def init(cmd, args):
 def check_package(pkg: str):
     if not pkg.__contains__("/"):
         global package
-        package = "NoahOnFyre/" + pkg
+        package = "noahonfyre/" + pkg
 
 
 def menu():
     os.system("cls")
     os.system("title Nitro - Async Package Manager")
     print(Fore.GREEN + "     _   __ _  __")
-    print(Fore.GREEN + "    / | / /(_)/ /_ _____ ____")
+    print(Fore.GREEN + "    / | / /(_)/ /_ _____ ____" + " "*4 + Fore.LIGHTBLACK_EX + "Python: " + Fore.GREEN + platform.python_version())
     print(Fore.GREEN + "   /  |/ // // __// ___// __ \\" + " "*3 + Fore.LIGHTBLACK_EX + "Packages: " + Fore.GREEN + str(len(os.listdir(out_dir))) + " package" + Fore.LIGHTBLACK_EX + "(s)" + Fore.GREEN + ".")
     print(Fore.GREEN + "  / /|  // // /_ / /   / /_/ /" + " "*3 + Fore.LIGHTBLACK_EX + "Version: " + Fore.GREEN + version.replace(".", Fore.LIGHTBLACK_EX + "." + Fore.GREEN))
-    print(Fore.GREEN + " /_/ |_//_/ \\__//_/    \\____/")
+    print(Fore.GREEN + " /_/ |_//_/ \\__//_/    \\____/" + " "*4 + Fore.LIGHTBLACK_EX + "Made by: " + Fore.GREEN + "NoahOnFyre")
+
+
+async def main():
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    if len(sys.argv) > 1:
+        args = sys.argv
+        cmd = args[1]
+        args.remove(sys.argv[0])
+        args.remove(cmd)
+        print()
+        await handle_command(cmd=cmd, args=args)
+        sys.exit(0)
+
+    menu()
+    while True:
+        try:
+            print()
+            args = input(Fore.LIGHTBLACK_EX + "\\\\" + Fore.GREEN + "nitro" + Fore.LIGHTBLACK_EX + " ~ " + Fore.RESET + "nitro ").split(" ")
+            print()
+            cmd = args[0]
+            args.remove(cmd)
+        except KeyboardInterrupt:
+            sys.exit(0)
+        await handle_command(cmd=cmd, args=args)
 
 
 path = os.path.dirname(os.path.abspath(sys.argv[0]))
 version = "1.0.0"
 out_dir = str(Path.home()) + "\\.nitro"
 
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-
-if len(sys.argv) > 1:
-    args = sys.argv
-    cmd = args[1]
-    args.remove(sys.argv[0])
-    args.remove(cmd)
-    print()
-    init(cmd=cmd, args=args)
-    sys.exit(0)
-
-menu()
-while True:
-    try:
-        print()
-        args = input(Fore.LIGHTBLACK_EX + "\\\\" + Fore.GREEN + "nitro" + Fore.LIGHTBLACK_EX + " ~ " + Fore.RESET + "nitro ").split(" ")
-        print()
-        cmd = args[0]
-        args.remove(cmd)
-    except KeyboardInterrupt:
-        sys.exit(0)
-    init(cmd=cmd, args=args)
+asyncio.run(main())
