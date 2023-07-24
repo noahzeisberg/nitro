2c6efcd0e86ed46b93cf65a08e6cd427e7df2f1d
NoahOnFyre <noahonfyre@gmail.com>
2023-07-25 00:56:55 +0200

    Added comments to setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,11 @@
 @echo off
 
+rem Install requirements.
 title Installing requirements...
 echo Installing requirements...
 pip install -r requirements.txt
 
+rem Copy data to user directory.
 title Installing Nitro...
 echo Installing Nitro...
 copy %cd%\nitro.py %userprofile%
@@ -11,7 +13,9 @@ copy %cd%\nitro.py %userprofile%
 title Setup finished!
 echo Setup finished!
 
+rem cd into user directory.
 cd /d %userprofile%
 echo Preparing for first use...
 
+rem Start the CLI.
 nitro
