903dcaad1ded4b4874968d531ffb42d7c0bfb2d6
NoahOnFyre <noahonfyre@gmail.com>
2023-08-04 00:16:54 +0200

    Added some more commands and basic utility.

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"os"
+	"strconv"
 
 	"github.com/TwiN/go-color"
+	title "github.com/lxi1400/GoTitle"
 )
 
 //////////////////////
@@ -20,7 +22,27 @@ var (
 	core_dir    string = nitro_dir + "\\core"
 	package_dir string = nitro_dir + "\\packages"
 	temp_dir    string = nitro_dir + "\\temp"
+
+	command string
+	args    []string
 )
+
+func menu() {
+	dir_content, err := os.ReadDir(package_dir)
+
+	if err != nil {
+		print(prefix(2) + "The package directory couldn't be read.")
+	}
+
+	printR("\033[H\033[2J")
+	title.SetTitle("Nitro Package Manager - " + nitro_dir)
+	print(color.Green + "     _   __ _  __")
+	print(color.Green + "    / | / /(_)/ /_ _____ ____ ")
+	print(color.Green + "   /  |/ // // __// ___// __ \\" + GRAY + "   Version: " + color.Green + "1.0.0")
+	print(color.Green + "  / /|  // // /_ / /   / /_/ /" + GRAY + "   Packages: " + color.Green + strconv.Itoa(len(dir_content)))
+	print(color.Green + " /_/ |_//_/ \\__//_/    \\____/ " + GRAY + "   Made by: " + color.Green + "NoahOnFyre")
+	print("")
+}
 
 ///////////////////
 // Main function //
@@ -36,24 +58,23 @@ func main() {
 		temp_dir,
 	})
 
-	print(color.Green + "     _   __ _  __")
-	print(color.Green + "    / | / /(_)/ /_ _____ ____ ")
-	print(color.Green + "   /  |/ // // __// ___// __ \\" + GRAY + "   Version: " + color.Green + "1.0.0")
-	print(color.Green + "  / /|  // // /_ / /   / /_/ /" + GRAY + "   Packages: " + color.Green + "")
-	print(color.Green + " /_/ |_//_/ \\__//_/    \\____/ " + GRAY + "   Made by: " + color.Green + "NoahOnFyre")
-	print("")
-
-	var command string
-	var args []string
+	menu()
 
 	args = args
 
 	for {
 		command, args = commandInput()
+		print("")
 
 		for _, props := range commandList {
 			if props.name == command {
-				props.run()
+				if len(args) == props.args {
+					props.run()
+					print("")
+				} else {
+					print(prefix(2) + "Unexpected arguments!")
+					print("")
+				}
 			}
 		}
 	}
--- networking.go
+++ networking.go
@@ -4,28 +4,46 @@ import (
 	"context"
 	"io/fs"
 	"os"
+	"strconv"
 	"strings"
 
+	"github.com/TwiN/go-color"
 	"github.com/google/go-github/github"
 )
 
 var (
-	client = github.NewClient(nil)
+	gitHubClient = github.NewClient(nil)
 )
 
 func fetchRepo(reponame string) {
 	owner := strings.Split(reponame, "/")[0]
 	repo := strings.Split(reponame, "/")[1]
 	pkg_name := package_dir + "\\" + owner + "-" + repo + "\\"
-	os.Mkdir(pkg_name, fs.ModeDir)
-	_, directoryContent, _, err := client.Repositories.GetContents(context.Background(), owner, repo, "", &github.RepositoryContentGetOptions{})
-
-	for _, content := range directoryContent {
-		os.Create(pkg_name + *content.Name)
+	if exists(pkg_name) {
+		print(prefix(2) + "The package is already installed!")
+		return
 	}
+	os.Mkdir(pkg_name, fs.ModeDir)
+	_, directoryContent, response, err := gitHubClient.Repositories.GetContents(context.Background(), owner, repo, "", &github.RepositoryContentGetOptions{})
 
 	if err != nil {
 		print(prefix(2) + "Repository not found!")
 		return
+	}
+
+	for _, contentProperties := range directoryContent {
+		if *contentProperties.Type != "file" {
+			continue
+		}
+		file, _ := os.Create(pkg_name + *contentProperties.Name)
+
+		print(prefix(0) + "Fetching " + color.Green + *contentProperties.Name + color.Reset + "... " + GRAY + "(" + strconv.Itoa(contentProperties.GetSize()) + " Bytes" + ")" + GRAY + " - " + color.Green + response.Status)
+		bytes_written, err := file.Write([]byte(*contentProperties.Content))
+		if err != nil {
+			print(prefix(2) + "Error while writing file: " + err.Error())
+			return
+		}
+		print(prefix(0) + "Wrote " + strconv.Itoa(bytes_written) + " bytes to " + file.Name())
+		file.Close()
 	}
 }
